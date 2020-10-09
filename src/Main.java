86ad0a9bb8778a4591d0befabb68d0b0a89d68ca
hiroto-mac <hirokyo11@outlook.jp>
2020-10-10 04:26:28 +0900

    モンスター追加をMonsterZooへ

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.util.List;
-import java.util.ArrayList;
-
 public class Main {
 	static User usr = new User();
 	static MonsterZoo pz = new MonsterZoo();
 
 	public static void main(String[] args) {
 
-		setMonsterZukan();
+		pz.setMonsterZukan();
 
 		//1000ミリ秒（1秒）ずつ止まりながらpz.move()を呼び出し続ける
 		//手持ちのボールが無くなったら終了
@@ -33,40 +30,6 @@ public class Main {
 				System.out.println(usr.getUserMonster().get(i)+"を捕まえた．");
 			}
 		}
-	}
-
-	//テスト用のモンスターデータを登録するメソッド
-	public static void setMonsterZukan(){
-		// String tempMonster[] = new String[22];
-		List<String> tempMonster = new ArrayList<String>();
-
-		// double tempMonsterRare[] = new double[22];
-		List<Double> tempMonsterRare = new ArrayList<Double>();
-		tempMonster.add("イガキン");	tempMonsterRare.add(9.0);
-		tempMonster.add("ナマチュウ");	tempMonsterRare.add(3.0);
-		tempMonster.add("イノウエン");	tempMonsterRare.add(1.0);
-		tempMonster.add("リョージィ");	tempMonsterRare.add(2.0);
-		tempMonster.add("アキモトン");	tempMonsterRare.add(5.0);
-		tempMonster.add("ゴージマ");	tempMonsterRare.add(4.0);
-		tempMonster.add("チュウデン");	tempMonsterRare.add(6.0);
-		tempMonster.add("ヅカホン");	tempMonsterRare.add(8.0);
-		tempMonster.add("ニシムラー");	tempMonsterRare.add(7.0);
-		tempMonster.add("サコーデン");	tempMonsterRare.add(2.0);
-		tempMonster.add("ウッチー");	tempMonsterRare.add(5.0);
-		tempMonster.add("ハヤッシー");	tempMonsterRare.add(4.0);
-		tempMonster.add("キーチー");	tempMonsterRare.add(3.0);
-		tempMonster.add("リョクン");	tempMonsterRare.add(1.0);
-		tempMonster.add("デコポン");	tempMonsterRare.add(6.0);
-		tempMonster.add("カミサン");	tempMonsterRare.add(5.0);
-		tempMonster.add("シスイ");	    tempMonsterRare.add(1.0);
-		tempMonster.add("ジョナ"); 	    tempMonsterRare.add(7.0);
-		tempMonster.add("ギダギダ");	tempMonsterRare.add(2.0);
-		tempMonster.add("ミッツー");	tempMonsterRare.add(8.0);
-		tempMonster.add("ゾエサン");	tempMonsterRare.add(5.0);
-		tempMonster.add("キタバー");	tempMonsterRare.add(3.0);
-
-		pz.setMonsterZukan(tempMonster);
-		pz.setMonsterRare(tempMonsterRare);
 	}
 
 }
--- src/MonsterZoo.java
+++ src/MonsterZoo.java
@@ -16,4 +16,36 @@ public class MonsterZoo {
 	public void setMonsterRare(List<Double> monsterRare) {
 		MonsterZoo.monsterRare = monsterRare;
 	}
+
+	//テスト用のモンスターデータを登録するメソッド
+	public void setMonsterZukan(){
+		List<String> tempMonster = new ArrayList<String>();
+
+		List<Double> tempMonsterRare = new ArrayList<Double>();
+		tempMonster.add("イガキン");	tempMonsterRare.add(9.0);
+		tempMonster.add("ナマチュウ");	tempMonsterRare.add(3.0);
+		tempMonster.add("イノウエン");	tempMonsterRare.add(1.0);
+		tempMonster.add("リョージィ");	tempMonsterRare.add(2.0);
+		tempMonster.add("アキモトン");	tempMonsterRare.add(5.0);
+		tempMonster.add("ゴージマ");	tempMonsterRare.add(4.0);
+		tempMonster.add("チュウデン");	tempMonsterRare.add(6.0);
+		tempMonster.add("ヅカホン");	tempMonsterRare.add(8.0);
+		tempMonster.add("ニシムラー");	tempMonsterRare.add(7.0);
+		tempMonster.add("サコーデン");	tempMonsterRare.add(2.0);
+		tempMonster.add("ウッチー");	tempMonsterRare.add(5.0);
+		tempMonster.add("ハヤッシー");	tempMonsterRare.add(4.0);
+		tempMonster.add("キーチー");	tempMonsterRare.add(3.0);
+		tempMonster.add("リョクン");	tempMonsterRare.add(1.0);
+		tempMonster.add("デコポン");	tempMonsterRare.add(6.0);
+		tempMonster.add("カミサン");	tempMonsterRare.add(5.0);
+		tempMonster.add("シスイ");	    tempMonsterRare.add(1.0);
+		tempMonster.add("ジョナ"); 	    tempMonsterRare.add(7.0);
+		tempMonster.add("ギダギダ");	tempMonsterRare.add(2.0);
+		tempMonster.add("ミッツー");	tempMonsterRare.add(8.0);
+		tempMonster.add("ゾエサン");	tempMonsterRare.add(5.0);
+		tempMonster.add("キタバー");	tempMonsterRare.add(3.0);
+
+		setMonsterZukan(tempMonster);
+		setMonsterRare(tempMonsterRare);
+	}
 }
